bb91c58827b4d4a1cf4b3b28c89c80aeb6d0795e
sultanmaa0 <136260941+sultanmaa0@users.noreply.github.com>
2024-05-29 11:39:20 +0700

    Update main.sh

--- LAVA1/main.sh
+++ LAVA1/main.sh
@@ -3,20 +3,20 @@
 while true; do
     # Ethereum
     echo "Running Ethereum script..."
-    /bin/bash /root/LAVA1/ethereum.sh "https://eth1.lava.build/lava-referer-716b858a-1626-4ba2-864a-de1d868d0de9/"
+    /bin/bash /root/panas/LAVA1/ethereum.sh "https://eth1.lava.build/lava-referer-716b858a-1626-4ba2-864a-de1d868d0de9/"
 
     # StarkNet
     echo "Running StarkNet script..."
-    /bin/bash /root/LAVA1/starknet.sh "https://rpc.starknet.lava.build/lava-referer-716b858a-1626-4ba2-864a-de1d868d0de9/"
+    /bin/bash /root/panas/LAVA1/starknet.sh "https://rpc.starknet.lava.build/lava-referer-716b858a-1626-4ba2-864a-de1d868d0de9/"
 
     # Axelar
     echo "Running Axelar script..."
-    /bin/bash /root/LAVA1/axelar.sh "https://tm.axelar.lava.build/lava-referer-716b858a-1626-4ba2-864a-de1d868d0de9/"
+    /bin/bash /root/panas/LAVA1/axelar.sh "https://tm.axelar.lava.build/lava-referer-716b858a-1626-4ba2-864a-de1d868d0de9/"
 
     # NEAR Protocol
     echo "Running NEAR Protocol script..."
-    /bin/bash /root/LAVA1/near.sh "https://near.lava.build/lava-referer-716b858a-1626-4ba2-864a-de1d868d0de9/"
+    /bin/bash /root/panas/LAVA1/near.sh "https://near.lava.build/lava-referer-716b858a-1626-4ba2-864a-de1d868d0de9/"
 
     # Wait for 5 minutes before next iteration
-    sleep 30  # 300 seconds = 5 minutes
+    sleep 20  # 300 seconds = 5 minutes
 done
